532012125647a39e7e375f2e6e74c2bbb4dba6d1
Nicole <wolfn@mail.nmc.edu>
2024-02-08 16:47:22 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GithubPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
